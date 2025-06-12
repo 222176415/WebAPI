83d365b1f13ff011a3d99f243e24fbde7e10654f
Themba  Ntimane <141935793+222176415@users.noreply.github.com>
2025-06-12 20:20:56 +0200

    entities
    Defining Entities and Db Context

---
+++ EmployeeAdminPortal/Database/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        //public ApplicationDbContext()
+        //{
+
+        //}
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected ApplicationDbContext()
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace EmployeeAdminPortal.Models.Entities
+{
+    public class Employee
+    {
+        public  Guid Id { set; get; }
+        public required string  name { set; get; }
+        public string? surname{ set; get; }
+        public required string  email { set; get; }
+        public required decimal salary { get;set }
+    }
+}
