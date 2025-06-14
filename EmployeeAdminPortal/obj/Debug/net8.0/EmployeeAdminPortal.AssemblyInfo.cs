8bae72ed78945bcbaf7b3db4747c3248261fa3c7
Themba  Ntimane <141935793+222176415@users.noreply.github.com>
2025-06-14 07:50:27 +0200

    update

--- EmployeeAdminPortal/obj/Debug/net8.0/EmployeeAdminPortal.AssemblyInfo.cs
+++ EmployeeAdminPortal/obj/Debug/net8.0/EmployeeAdminPortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f1373f4a104bdda62f95bf742653c94c0c317f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+244c3a99c568ee66e7183d5800e7b4d700085166")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
