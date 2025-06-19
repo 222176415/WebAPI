e03237055410650085233b3f30bbb7440fc43733
Themba  Ntimane <141935793+222176415@users.noreply.github.com>
2025-06-19 20:07:20 +0200

    Update Employyee Entity

--- EmployeeAdminPortal/Models/Entities/Employee.cs
+++ EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -4,7 +4,7 @@ namespace EmployeeAdminPortal.Models.Entities
 {
     public class Employee
     {
-        public  Guid EmployeeId { set; get; }
+        public  string EmployeeId { set; get; }
         public required string IdNumber { set; get; }
         public required string  Name { set; get; }
         public string? Surname{ set; get; }
