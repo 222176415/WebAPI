3e93dd318c09e37ef3dad508038889c273cf924d
Themba  Ntimane <141935793+222176415@users.noreply.github.com>
2025-06-20 20:47:43 +0200

    Update

---
+++ EmployeeAdminPortal/Models/UpdateEmployeeDto.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeeAdminPortal.Models
+{
+    public class UpdateEmployeeDto_
+    {
+
+        public required string IdNumber { set; get; }
+        public required string Name { set; get; }
+        public string? Surname { set; get; }
+        public string? Gender { set; get; }
+        public string? phone { set; get; }
+        public required string Email { set; get; }
+        public required decimal Salary { get; set; }
+    }
+}
--- EmployeeAdminPortal/obj/Debug/net8.0/EmployeeAdminPortal.AssemblyInfo.cs
+++ EmployeeAdminPortal/obj/Debug/net8.0/EmployeeAdminPortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81e3f6f600e08f2fb53830377ecd3690a2c84110")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a2c901551d0ec582ec8599089b5edf5d564641a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
